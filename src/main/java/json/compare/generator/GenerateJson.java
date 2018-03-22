5a363c3de244055a81ef06eae4e2d0f410d7ef6b
k.poltoratskyi <k.poltoratskyi@controlpay.com>
2018-03-22 14:30:59 +0200

    ~

--- src/main/java/json/compare/generator/GenerateJson.java
+++
@@ -1,5 +0,0 @@
-package json.compare.generator;
-
-public class GenerateJson {
-
-}
--- src/test/java/json/compare/RandomFieldsTest.java
+++ src/test/java/json/compare/RandomFieldsTest.java
@@ -2,15 +2,24 @@ package json.compare;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import json.compare.common.RateCardField;
 import json.compare.generator.source.RateCardFieldSource;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +47,51 @@ public class RandomFieldsTest {
         long timeElapsed = System.currentTimeMillis() - start;
 
         System.out.println("Parsing " + QUANTITY + " fields json with Jackson took " + timeElapsed + " (ms)");
+    }
+
+    @Test
+    public void testParsingWithJGson() throws IOException {
+        List<RateCardField> parsedFields = new ArrayList<>();
+        List<String> jsonList = getJsonList();
+        Gson gson = new GsonBuilder().create();
+
+        long start = System.currentTimeMillis();
+        for (String json : jsonList) {
+            parsedFields.add(gson.fromJson(json, RateCardField.class));
+        }
+        long timeElapsed = System.currentTimeMillis() - start;
+
+        System.out.println("Parsing " + QUANTITY + " fields json with Gson took " + timeElapsed + " (ms)");
+    }
+
+    @Test
+    public void testParsingWithJsonSimple() throws IOException, ParseException {
+        List<JSONObject> parsedFields = new ArrayList<>();
+        List<String> jsonList = getJsonList();
+        JSONParser jsonParser = new JSONParser();
+
+        long start = System.currentTimeMillis();
+        for (String json : jsonList) {
+            parsedFields.add((JSONObject) jsonParser.parse(json));
+        }
+        long timeElapsed = System.currentTimeMillis() - start;
+
+        System.out.println("Parsing " + QUANTITY + " fields json with JSON.Simple took " + timeElapsed + " (ms)");
+    }
+
+    @Test
+    public void testParsingWithJsonP() throws IOException {
+        List<JsonObject> parsedFields = new ArrayList<>();
+        List<String> jsonList = getJsonList();
+
+        long start = System.currentTimeMillis();
+        for (String json : jsonList) {
+            JsonReader reader = Json.createReader(new StringReader(json));
+            parsedFields.add(reader.readObject());
+        }
+        long timeElapsed = System.currentTimeMillis() - start;
+
+        System.out.println("Parsing " + QUANTITY + " fields json with Jsonp took " + timeElapsed + " (ms)");
     }
 
     private List<String> getJsonList() throws JsonProcessingException {
